507d1e4775bdf6f9848fd76772dcb2b6e14ec734
Formalhalt <89222800+Formalhalt@users.noreply.github.com>
2021-09-15 20:56:25 -0400

    Add files via upload
    update: 
    added win screen
    added new enemy (not yet functional apart from visuals)
    music removed (temporary) 
    scores are currently lowered until cause i forgot to change them back before closing pycharm

--- snakedodge.py
+++ snakedodge.py
@@ -6,13 +6,10 @@ from pygame import mixer
 # Starts pygame
 pygame.init()
 
-
 # Creates window for game
 screen = pygame.display.set_mode((400, 600))
 clock = pygame.time.Clock()
 # music bg
-mixer.music.load('back ground music.mp3')
-mixer.music.play(-1)
 # Title and logo
 pygame.display.set_caption("Snake Dodge")
 icon = pygame.image.load('snek.png')
@@ -20,7 +17,6 @@ pygame.display.set_icon(icon)
 
 game_win = pygame.image.load('winner.jpg')
 go_screen = pygame.image.load("GAAME OVER.jpg")
-
 
 # skins
 
@@ -40,6 +36,12 @@ Enemy1Y = (random.randint(400, 550))
 Enemy1X_change = (random.randint(1, 2))
 Enemy1Y_change = 0
 
+EnemyE2Img = (pygame.image.load('ENEMEy.png'))
+EnemyE2X = -10
+EnemyE2Y = 0
+EnemyE2X_change = 0
+EnemyE2Y_change = 0
+
 # bullets
 
 Bullet1Img = pygame.image.load('wepunee.png')
@@ -57,11 +59,19 @@ Bullet2Y_change = 0.4
 Bullet2X_change = 0
 bullet2_state = "GO"
 
+BulE2img = pygame.image.load('bullete2.png')
+BulE2img = pygame.transform.scale(BulE2img, (40, 40))
+BulE2X = EnemyE2X
+BulE2Y = 0
+BulE2X_change = 0.5
+BulE2Y_change = 0
+BulE2_state = "kiz"
+
 # score
 
 score_value = 0
 font = pygame.font.Font('freesansbold.ttf', 32)
-textX = 189
+textX = 179
 textY = 30
 
 game_over = pygame.font.Font('freesansbold.ttf', 100)
@@ -70,6 +80,12 @@ game_over = pygame.font.Font('freesansbold.ttf', 100)
 def s_score(x, y):
     score = font.render(str(score_value), True, (255, 255, 255))
     screen.blit(score, (x, y))
+
+
+def e2_fire(x, y):
+    global BulE2_state
+    BulE2_state = "kiz"
+    screen.blit(BulE2img, (x + 1, y + 0))
 
 
 def fire_player(x, y):
@@ -82,6 +98,10 @@ def fire_bullet(x, y):
     global bullet_state
     bullet_state = "fire"
     screen.blit(Bullet1Img, (x + 11, y + 0))
+
+
+def enemy2(x, y):
+    screen.blit(EnemyE2Img, (x, y))
 
 
 def enemy(x, y):
@@ -115,16 +135,14 @@ def player_hit_enemy_col(Enemy1X, Enemy1Y, Bullet2X, Bullet2Y):
     else:
         return False
 
-# Game loop
 
+# Game loop
 
 running = True
 
 while running:
 
     screen.fill((174, 198, 207))
-
-
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -139,20 +157,19 @@ while running:
                 playerX_change = 0.3
             if event.key == pygame.K_UP:
                 playerY_change = -0.3
+                EnemyE2Y_change = -0.2
             if event.key == pygame.K_DOWN:
                 playerY_change = 0.3
+                EnemyE2Y_change = 0.2
+
             if event.key == pygame.K_SPACE:
                 if bullet2_state == "GO":
-                    bullet_sound = mixer.Sound('HS.mp3')
-                    bullet_sound.play()
                     Bullet2X = playerX
                     fire_player(playerX, Bullet2Y)
 
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_SPACE:
                 if bullet_state == "ready":
-                    bullet_sound = mixer.Sound('HS.mp3')
-                    bullet_sound.play()
                     Bullet1X = Enemy1X
                     fire_bullet(Enemy1X, Bullet1Y)
 
@@ -181,6 +198,24 @@ while running:
     elif Enemy1X >= 336:
         Enemy1X_change = -0.2
 
+    if score_value >= 5:
+
+        EnemyE2Y += EnemyE2Y_change
+        if EnemyE2Y <= 0:
+            EnemyE2Y_change = 0.2
+        elif EnemyE2Y >= 300:
+            EnemyE2Y_change = -0.2
+
+       # if BulE2X <= 290:
+        #    BulE2X = EnemyE2Y
+         #   bullet_state = "kiz"
+
+       # if bullet_state == "kiz":
+        #    e2_fire(BulE2X, BulE2Y)
+         #   BulE2X -= BulE2X_change
+
+        enemy2(EnemyE2X, EnemyE2Y)
+
     # Bullet movement
 
     if Bullet1Y <= 10:
@@ -203,8 +238,6 @@ while running:
 
     B_collision = bullet_collision(Bullet1X, Bullet1Y, Bullet2X, Bullet2Y)
     if B_collision:
-        EXP = mixer.Sound('HS.mp3')
-        EXP.play()
         Bullet2Y = playerY
         Bullet1Y = Enemy1Y
         bullet_state = "ready"
@@ -213,8 +246,6 @@ while running:
 
     player_hit_enemy = player_hit_enemy_col(Enemy1X, Enemy1Y, Bullet2X, Bullet2Y)
     if player_hit_enemy:
-        PXE = mixer.Sound('HS.mp3')
-        PXE.play()
         Bullet2Y = playerY
         Enemy1X = random.randint(20, 400)
         bullet2_state = "GO"
@@ -226,13 +257,16 @@ while running:
 
     game_oo = game_o(Bullet1X, Bullet1Y, playerX, playerY)
     if game_oo == True:
-        clock.tick(60000)
-        screen.blit(go_screen, (0, 0))
+        playerX = 170
+        playerY = 175
+        Enemy1X = (random.randint(20, 400))
+        Enemy1Y = (random.randint(400, 550))
         score_value -= score_value
 
-    if score_value == 200:
+    if score_value >= 10:
+        pygame.display.set_mode((400, 600))
+        screen.fill((0, 0, 0))
         screen.blit(game_win, (0, 0))
-        score_value -= score_value
 
     clock.tick(600000000000000000000)
     pygame.display.update()
